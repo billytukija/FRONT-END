c4afccf2c197255bcc966f502c7b2da8c91f565a
billytukija <billytukija@gmail.com>
2018-04-01 17:38:31 +0100

    REACT
    Simple ract project example

---
+++ standalone_first_app_react/src/Navbar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+
+class Navbar extends Component
+{
+  render()
+  {
+      return(
+          <div className = "Navbar">
+                this is the standalone Navbar
+          </div>
+      );
+  }
+}
+
+export default Navbar;
