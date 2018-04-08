537dd18efd6c058be03fb16cfc56d30bf615c602
billytukija <billytukija@gmail.com>
2018-04-08 08:26:07 +0100

    Google Recaptcha
    Simple project teaching how to secure your systems using Google Recaptcha

---
+++ MVCNewGoogleRecaptcha/MVCNewGoogleRecaptcha/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace MVCNewGoogleRecaptcha.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FormSubmit()
+        {
+            var response = Request["g-recaptcha-response"];
+            string secretKey = "";
+            var client = new WebClient();
+            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey,response));
+            var obj = JObject.Parse(result);
+            var status = (bool)obj.SelectToken("success");
+            ViewBag.Message = status ? "Google reCaptcha validation succes" : "Google reCaptcha validation failled";
+            
+            return View("Index");
+        }
+    }
+}
